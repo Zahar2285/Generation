3d54321b653134d0a7d1cd73f8fd20c7f6b6ea08
Zahar2285 <Zahar2285@mail.ru>
2025-01-14 22:23:36 +0300

    Add files via upload

--- Module_9_6.py
+++ Module_9_6.py
@@ -3,15 +3,11 @@ def all_variants(text):
     Генератор all_variants принимает строку text и возвращает все возможные
     подпоследовательности строки (включая одиночные символы, пары символов и т. д.).
     """
-    for start in range(len(text)):
-
-        for end in range(start + 1, len(text) + 1):
-
-            yield text[start:end]
-
+    n = len(text)
+    for length in range(1, n + 1):
+        for start in range(n - length + 1):
+            substring = text[start:start + length]
+            yield substring
 a = all_variants("abc")
 for i in a:
-
    print(i)
-
-
